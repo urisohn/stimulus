77e13496ef1fb61f811f6f4825653ab9d7437fa9
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2024-02-07 15:49:23 +0100

    onload bug

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,9 @@
 
   .onLoad <- function(libname, pkgname) {
-  packageStartupMessage('loading stimulus')
-  message('--------------------')
+  
+  }
+  
+  
   #1. Attaching 
     #' @importFrom utils packageVersion compareVersion
      
@@ -9,12 +11,12 @@
       
      #Startup msgs
       #Version and URL for website
-          packageStartupMessage ("Attached: 'stimulus' (Version: ",packageVersion('groundhog'),  ")") 
+          packageStartupMessage ("Attached: 'stimulus' (Version: ",packageVersion('stimulus'),  ")") 
 
       #While developing:
          packageStartupMessage ("#######################################################\n",
                               "This DEV version: 2024 02 07 - 3.35 (Barcelona time)")
       
       } #End on attach
-  }
+  
    
