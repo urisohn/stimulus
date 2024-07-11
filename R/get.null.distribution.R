ea84eb42f39b5c25a8a96eb616c01bb2e80a0944
Uri Simonsohn <urisohn@gmail.com>
2024-07-11 13:30:11 +0200

    adjust null with lm | flip sign | t-test

--- R/get.null.distribution.R
+++ R/get.null.distribution.R
@@ -1,37 +1,32 @@
- get.null.distribution = function(df,  dv, stimulus, condition, participant,simtot=100)
+ get.null.distribution = function(df,  dv, stimulus, condition, participant,simtot=100,flip.sign)
   {
     
-  #Model to residualize var controlling for stimuli, with or without participant id
-    df=as.data.frame(df)
-    df2=df[,0]
-    df2$dv        = df[,dv]
-    df2$condition = df[,condition]
-    df2$stimulus  = df[,stimulus]
-    df2$participant=""
-    if (participant!='') df2$participant=df[,participant]
-
-    df2=as.data.frame(df2)    
+    #Model to residualize var controlling for stimuli, with or without participant id
+      #df=as.data.frame(df)
+      #df2=df[,0]
+      #df2$dv        = df[,dv]
+      #df2$condition = df[,condition]
+      #df2$stimulus  = df[,stimulus]
     
     
     #Residualize stimulus effects
-      if (participant!='')     m0 = lme4::lmer(dv~(1|stimulus)+(1|participant),data=df2)
-      if (participant=='')     m0 = lme4::lmer(dv~(1|stimulus)                ,data=df2)
-    
-      df2$r = residuals(m0)
-                    
+      #m0 = lme4::lmer(dv~(1|stimulus),data=df2)
+      m0=lm(df[,dv]~factor(df[,stimulus]))
+      df$r=residuals(m0)
+      
     #Compute means on residuals
-      means.all=matrix(nrow=simtot,ncol=length(unique(df2$stimulus)))
+      means.all=matrix(nrow=simtot,ncol=length(unique(df[,stimulus])))
       for (k in 1:simtot)
       {
         
       #Shuffle item within condition
-        df2$stimulus_shuffled  <- ave(df2$stimulus, df2$condition, FUN = function(x) sample(x))
+        df[,paste0(stimulus,"_shuffled")]  <- ave(df2$stimulus, df2$condition, FUN = function(x) sample(x))
       
       #Get means of residualized dv on shuffled stimuli
-        means.k   = get.means.condition(df=df2,dv='r',stimulus='stimulus_shuffled',condition='condition',participant='',sort.by='')
-        
+        tk   = get.means.condition(df=df,dv='r',stimulus=paste0(stimulus,"_shuffled"),condition='condition',sort.by='',flip.sign = flip.sign)
+            
       #Extract estimates
-        means.all[k,] = sort(means.k$effect)
+        means.all[k,] = sort(tk$effect)
         
       #Counter
         if (k%%50==0) cat('...',k)
