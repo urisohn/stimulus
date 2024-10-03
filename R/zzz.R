98f627700526a0a0583b2d02ee63e0046ad60d93
Uri Simonsohn <56013245+urisohn@users.noreply.github.com>
2024-10-03 12:49:38 +0200

    version uptick

--- R/zzz.R
+++ R/zzz.R
@@ -16,7 +16,7 @@
       #While developing:
          packageStartupMessage ("#######################################################\n",
                               "DEVELOPMENT VERSION - DO NOT USE IN PUBLISHABLE PAPERS\n",
-                              "This Version 2024 08 09 - 03.50PM\n",
+                              "This Version 2024 10 03 - 10.53AM\n",
                               "***  Subject to breaking changes & likely errors  ***")
       
       } #End on attach
